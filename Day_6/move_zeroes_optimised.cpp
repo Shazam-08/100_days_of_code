4dd07961b4a96ab526a631566e6bf8eeede2777d
SHUBHAM KUMAR <shubhamkumar@SHUBHAMs-MacBook-Air.local>
2023-08-12 20:32:05 +0530

    move zeroes to end optimised using vectors

---
+++ Day_6/move_zeroes_optimised.cpp
@@ -0,0 +1,36 @@
+// This is the optimised code for the question to move the zeroes at the right side of an array with vector iterations used for displaying the elements
+
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<int> moveZeroes(int n,vector<int>arr){
+    int j =-1;
+    for(int i=0;i<n;i++){
+        if(arr[i]==0){
+            j=i;
+            break;
+        }
+    }
+    
+    if(j==-1)
+    return arr;
+    
+    for(int i=j+1;i<n;i++){
+        if(arr[i]!=0){
+            swap(arr[i],arr[j]);
+            j++;
+        }
+    }return arr;
+    
+}
+
+int main(){
+    vector<int>arr= {1, 0, 2, 3, 2, 0, 0, 4, 5, 1};
+    int n=10;
+    vector<int>result = moveZeroes(n,arr);
+    for(auto&it:result){
+        cout<<it<<" ";
+    }
+    cout<<endl;
+    return 0;
+}
